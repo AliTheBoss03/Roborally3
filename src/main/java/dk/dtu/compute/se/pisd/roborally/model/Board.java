8a18d328959ba02667c7843520fb6cc963133809
Ali Masoud <96056143+AliTheBoss03@users.noreply.github.com>
2023-05-05 20:40:31 +0200

    JavaDoc kommentarer

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
@@ -179,7 +179,7 @@ public class Board extends Subject {
         }
     }
 
-    /**
+    /*
      * Returns the neighbour of the given space of the board in the given heading.
      * The neighbour is returned only, if it can be reached from the given space
      * (no walls or obstacles in either of the involved spaces); otherwise,
@@ -225,9 +225,6 @@ public class Board extends Subject {
         // the students, this method gives a string representation of the current
         // status of the game
 
-
-        // TODO V1: add the move count to the status message
-        // XXX: V2 changed the status so that it shows the phase, the current player and the number of steps
         return "Phase: " + getPhase().name() +
                 ", Player = " + getCurrentPlayer().getName() + ", Moves = " + getCurrentPlayer().getMoveCount();
     }
